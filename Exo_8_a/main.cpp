90ae35c2b0fa616bbcf091f67569b402aa45707b
Linus Gasser <linus.gasser@epfl.ch>
2022-12-07 21:12:00 +0100

    Solutions for 8a and 8b

--- Exo_8_a/main.cpp
+++ Exo_8_a/main.cpp
@@ -1,13 +1,23 @@
 #include <array>
 #include <iostream>
+#include <numeric>
 
 using namespace std;
 
 // Créer la méthode pour afficher un array ici.
+template <typename T, size_t N> void affichage(array<T, N> list) {
+  for (const auto &l: list){
+    cout << l << (&l == list.end() - 1 ? "" : ",");
+  }
+  cout << endl;
+}
 
 int main() {
   array<int, 4> a{2, 7, 1, 8};
   affichage(a);
+
+  array<int, 2> c{2, 7};
+  affichage(c);
 
   array<double, 8> b{1, 1, 2, 3, 5, 8, 13, 21};
   affichage(b);
--- Exo_8_b/main.cpp
+++ Exo_8_b/main.cpp
@@ -1,9 +1,34 @@
 #include <array>
 #include <iostream>
+#include <iterator>
 
 using namespace std;
 
-// Définir la class NTuple ici
+template <typename T, size_t N> class NTuple {
+  std::array<T, N> list;
+
+public:
+  NTuple();
+  NTuple(std::array<T, N> array);
+  void set(size_t index, T value);
+
+  void affichage();
+
+  NTuple &operator+=(const NTuple &lhs);
+
+  friend NTuple operator+(NTuple lhs, const NTuple &rhs) { return lhs += rhs; }
+
+  // Ou alors, en séparé:
+
+  template <typename T_, size_t N_>
+  friend NTuple<T_, N_> operator-(NTuple<T_, N_> lhs,
+                                  const NTuple<T_, N_> &rhs) {
+    for (size_t i = 0; i < N; i++) {
+      lhs.list.at(i) -= rhs.list.at(i);
+    }
+    return lhs;
+  }
+};
 
 // La sortie de `main` doit être:
 // 0,0,2,3
@@ -24,3 +49,36 @@ int main() {
   cout << endl;
   return 0;
 }
+
+template <typename T, size_t N>
+NTuple<T, N>::NTuple(): NTuple(array<T, N>()){};
+
+template <typename T, size_t N>
+NTuple<T, N>::NTuple(std::array<T, N> list): list(list){};
+
+
+template <typename T, size_t N> void NTuple<T, N>::set(size_t index, T value) {
+  list.at(index) = value;
+}
+
+template <typename T, size_t N> void NTuple<T, N>::affichage() {
+  for (const auto &l: list){
+    cout << l << (&l == list.end() - 1 ? "" : ",");
+  }
+  cout << endl;
+}
+
+template <typename T, size_t N>
+NTuple<T, N> &NTuple<T, N>::operator+=(const NTuple<T, N> &lhs) {
+  for (size_t i = 0; i < N; i++) {
+    list.at(i) += lhs.list.at(i);
+  }
+  return *this;
+}
+
+template <typename T, size_t N>
+NTuple<T, N> operator+(NTuple<T, N> lhs, const NTuple<T, N> &rhs) {
+  return lhs += rhs;
+}
+
+// Définition de la class NTuple ici
